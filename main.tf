496939d580a78277f528e9f1b2e0868b22b6f3d4
juan esteban colcombet <quinont@gmail.com>
2018-08-06 17:17:00 -0300

    sumando un main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "google" {
+  project     = "new-project-212112"
+  region      = "us-central1"
+}
+
+
+resource "google_compute_instance" "default" {
+  name         = "test-tf"
+  machine_type = "g1-small"
+  zone         = "us-central1-a"
+  
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-8"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+  }
+}
