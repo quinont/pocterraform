e2df1bc276c6d22e5b24ceef3acebed517747a9a
Juan Ignacio Giro <juan.giro@globant.com>
2018-08-09 14:49:07 -0300

    actualizando imagen del OS para pruebas

--- main.tf
+++ main.tf
@@ -5,13 +5,13 @@ provider "google" {
 
 
 resource "google_compute_instance" "default" {
-  name         = "test-tf"
+  name         = "test-tfa"
   machine_type = "g1-small"
   zone         = "us-central1-a"
   
   boot_disk {
     initialize_params {
-      image = "COS"
+      image = "debian-cloud/debian-8"
     }
   }
 
